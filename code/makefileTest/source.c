b363227d0f4bab9818b64e00d7fedcb3cec6201f
Neal Chapman <chapman.neal@gmail.com>
2011-08-12 19:39:53 -0700

    rewrote uCLinux's DMA API

---
+++ code/makefileTest/source.c
@@ -0,0 +1,12 @@
+#include "dir1/aux1.h"
+
+
+int main(void)
+{
+
+	int a = 0;
+	while(1);
+
+	return 0;
+
+}
--- code/synth/Hardware/BF548/DMA.h
+++ code/synth/Hardware/BF548/DMA.h
@@ -1,8 +1,26 @@
 #ifndef __DMA_H__
 #define __DMA_H__
 
-void configureDMA(uint16_t DMAChannel, uint16_t destination, uint16_t *startAddress, uint16_t direction, uint16_t wordSize, uint16_t interruptEnable, uint16_t flow, uint16_t size);
-void startDMA(uint16_t DMAChannel)
-void stopDMA(uint16_t DMAChannel);
+int requestDMA(uint32_t channel, const char *device_id);
+void freeDMA(uint32_t channel);
+void enableDMA(uint32_t channel);
+void disableDMA(uint32_t channel);
+
+void setDMAStartAddr(uint32_t channel, uint32_t addr);
+void setDMANextDescAddr(uint32_t channel, uint32_t addr);
+void setDMAXCount(uint32_t channel, uint16_t xCount);
+void setDMAXModify(uint32_t channel, uint16_t xModify);
+void setDMAYCount(uint32_t channel, uint16_t yCount);
+void setDMAYModify(uint32_t channel, uint16_t yModify);
+void setDMAConfig(uint32_t channel, uint16_t config);
+uint16_t setBFINDMAConfig(char direction, char flowMode, char intrMode, char DMAMode, char width);
+uint16_t getDMACurrIRQStatus(uint32_t channel);
+uint16_t getDMACurrXCount(uint32_t channel);
+uint16_t getDMACurrYCount(uint32_t channel);
+void setDMASG(uint32_t channel, struct dmasg_t *sg, uint32_t nrSG);
+void DMADisableIRQ(uint32_t channel);
+void DMAEnableIRQ(uint32_t channel);
+void clearDMAIRQStatus(uint32_t channel);
+uint32_t setDMACallback(uint32_t channel, dmaInterruptT callback, void *data);
 
 #endif //__DMA_H__
